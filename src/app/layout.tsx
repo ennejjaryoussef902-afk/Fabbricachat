1c6c85674276c83ef674a33d9ef5270427179f94
ennejjaryoussef902-afk <ennejjaryoussef902@gmail.com>
2025-12-27 00:03:30 +0100

    Fix: ignore lint and ts during build in config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import "@/styles/globals.css";
-
 import { Inter } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import React from "react";
 import { RecoilRoot } from "recoil";
-
 import ActiveStatus from "@/components/ActiveContext";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,6 +13,10 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }: { children: React.ReactNode }): React.JSX.Element {
 	return (
 		<html lang="en">
+			<head>
+				{/* Codice di verifica Google Search Console */}
+				<meta name="google-site-verification" content="eF9DTmRQ4OsHemaWs20G1oZ77FuWMoIfhTTczYWiDHY" />
+			</head>
 			<body className={`${inter.className}`}>
 				<SessionProvider>
 					<RecoilRoot>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ import React from "react";
 import dynamicImport from "next/dynamic";
 
 // Carichiamo AuthForm solo sul client, disabilitando il caricamento sul server
-const AuthForm = dynamicImport(() => import("@/components/AuthForm"), { 
-  ssr: false,
-  loading: () => <p>Caricamento...</p> 
+const AuthForm = dynamicImport(() => import("@/components/AuthForm"), {
+	ssr: false,
+	loading: () => <p>Caricamento...</p>,
 });
 
 export default function Home(): React.JSX.Element {
-  return (
-    <main>
-      <AuthForm />
-    </main>
-  );
+	return (
+		<main>
+			<AuthForm />
+		</main>
+	);
 }
